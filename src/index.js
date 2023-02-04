e5803d40443ef97dc4f4b54a77856d0e1624fc3a
unknown <zykov_rk@mail.ru>
2023-02-04 11:37:05 +0300

    старт

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const Card = function() {
+  return <div className="card">=_=</div>
+}
+
+ReactDOM.render(
+<>
+  <h1>Hello <sapn className="mark">React</sapn>!</h1>
+  <div class="box">
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  </div>
+</>,
+  document.querySelector("#root")
+)
